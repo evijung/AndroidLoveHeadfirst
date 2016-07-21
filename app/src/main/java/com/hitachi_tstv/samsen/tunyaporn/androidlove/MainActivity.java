f1d4873e6a6c56847603233bcdee9629388be2f1
evijung <evides01@gmail.com>
2016-07-21 15:01:47 +0700

    End for meeting android

--- app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/androidlove/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/androidlove/MainActivity.java
@@ -2,12 +2,26 @@ package com.hitachi_tstv.samsen.tunyaporn.androidlove;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    TextView detailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind widget
+        detailTextView = (TextView) findViewById(R.id.txtDetail);
+
+    }
+
+    public void btnClickMeClicked(View view) {
+
+        detailTextView.setVisibility(View.VISIBLE);
+
     }
 }
